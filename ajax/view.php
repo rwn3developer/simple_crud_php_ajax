787638c76a1ec13926b4b493beb39f8b08a07107
milan <rwn3developer11@gmail.com>
2022-12-23 09:35:35 +0530

    updatecode

--- ajax/view.php
+++ ajax/view.php
@@ -14,18 +14,18 @@
 
               </tr>";
 
-    while($row = mysqli_fetch_array($se))
-    {
-            $data .= "<tr>
-                         <td>".$row['name']."</td>
-                         <td>".$row['email']."</td>
-                         <td>".$row['password']."</td>
-                         <td>
-                            <a href='javascript:deletedata(".$row['id'].")'>Delete</a> ||
-                            <a href='javascript:editdata(".$row['id'].")'>Edit</a
-                        <td>
-                     </tr>";
-    }
+    // while($row = mysqli_fetch_array($se))
+    // {
+    //         $data .= "<tr>
+    //                      <td>".$row['name']."</td>
+    //                      <td>".$row['email']."</td>
+    //                      <td>".$row['password']."</td>
+    //                      <td>
+    //                         <a href='javascript:deletedata(".$row['id'].")'>Delete</a> ||
+    //                         <a href='javascript:editdata(".$row['id'].")'>Edit</a
+    //                     <td>
+    //                  </tr>";
+    // }
 
     echo $data;
 ?>
